0f4bf11d55da3e73da804564fb24e0a2366b620b
dhyey-R12 <rajparadhyey12@gmail.com>
2023-09-12 00:12:28 +0530

    design banner section

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function () {
+	// banner slider JS
+	$('.banner_slider').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		arrows: false,
+	});
+});
