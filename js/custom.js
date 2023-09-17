b7eb550cfb6af40512d046f1eff0a56011ba34bd
dhyey-R12 <rajparadhyey12@gmail.com>
2023-09-17 22:22:14 +0530

    Design Sections

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,24 @@
 jQuery(document).ready(function () {
 	// banner slider JS
-	$('.banner_slider').slick({
+	var $slider = $('.banner_slider');
+	// var $progressBar = $('.progress');
+	// var $progressBarLabel = $( '.slider__label' );
+
+	// $slider.on('afterChange', function(event, slick, currentSlide, nextSlide) {   
+	// 	var currentDot = $(".slick-dots .slick-active").index() + 1;
+	// 	var dots = $slider.find('.slick-dots li').length;
+	// 	var calc = (currentDot/dots) * 100;
+	// 	$progressBar
+	// 	.css('background-size', calc + '% 100%')
+	// 	.attr('aria-valuenow', calc );
+	// 	$progressBarLabel.text( calc + '% completed' );
+	// });
+
+	$slider.slick({
 		dots: false,
 		infinite: true,
 		speed: 300,
 		slidesToShow: 1,
-		adaptiveHeight: true,
 		arrows: false,
-	});
+	});  
 });
