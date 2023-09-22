b0a05d57f4def82f54850b7761af57caab21dba8
dhyey-R12 <rajparadhyey12@gmail.com>
2023-09-23 00:37:00 +0530

    Desgin Sections

--- js/custom.js
+++ js/custom.js
@@ -21,4 +21,36 @@ jQuery(document).ready(function () {
 		slidesToShow: 1,
 		arrows: false,
 	});  
+
+	// Producr slider JS
+	$('.product_slider').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '.product_prev_btn',
+		nextArrow: '.product_next_btn',
+		touchThreshold: 60,
+		responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+			}
+		}
+		]
+	});  
 });
